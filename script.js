89b60693b76bccfaa517b675bb141401e279d243
Claire <clairelogan16@gmail.com>
2021-06-12 13:52:54 -0700

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const linkedIn = document.getElementById('Linkedin-logo');
+
+function clickAlert() {
+    alert('You are now leaving my site for my LinkedIn page');
+}
+
+linkedIn.addEventListener('click', clickAlert);
